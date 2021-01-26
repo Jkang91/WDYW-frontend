adf7a17d8b735ba477e4d197623a83ff412bbc89
jkang91 <jkang34@gmail.com>
2021-01-26 11:50:46 -0500

    changes

---
+++ src/fetch.js
@@ -0,0 +1,8 @@
+//FETCH REQUESTS
+// function getRestaurants(){
+//     fetch("http://localhost:3000/restaurants")
+//         .then(resp => resp.json())
+//         .then(restaurantArray => {
+//             restaurantArray.forEach(restaurant => renderRestaurant(restaurant))
+//         })
+// }
